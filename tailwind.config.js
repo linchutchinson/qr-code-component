46ce8b9c8d3e95e205b0e0c22d9b0dabaccc9017
Lincoln Hutchinson <me@linchutchinson.com>
2023-02-11 16:25:55 -0800

    Finish design. Run prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
+    colors: {
+      white: "#fff",
+      lightgray: "hsl(212, 45%, 89%)",
+      grayblue: "hsl(220, 15%, 55%)",
+      darkblue: "hsl(218, 44%, 22%)",
+    },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+    },
+    fontSize: {
+      base: "15px",
+    },
     extend: {},
   },
   plugins: [],
